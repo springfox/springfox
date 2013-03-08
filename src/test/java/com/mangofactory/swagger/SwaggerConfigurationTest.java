e9906ae76ba445381cb74ac0e36b989bb66a8436
Dilip Krishnan <d_krishnan@containerstore.com>
2013-03-08 11:25:36 -0600

    Issue #41 - Fixed an issue that was not excluding controller urls correctly

---
+++ src/test/java/com/mangofactory/swagger/SwaggerConfigurationTest.java
@@ -0,0 +1,42 @@
+package com.mangofactory.swagger;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static com.google.common.collect.Lists.*;
+import static org.junit.Assert.*;
+
+public class SwaggerConfigurationTest {
+    private SwaggerConfiguration config;
+
+    @Before
+    public void setUp() throws Exception {
+        config = new SwaggerConfiguration();
+        config.setExcludedResources(newArrayList("/pets"));
+    }
+
+    @Test
+    public void whenDocumentationEndpointUriIsEmpty() {
+        assertFalse(config.isExcluded(""));
+    }
+
+    @Test
+    public void whenDocumentationEndpointUriIsNull() {
+        assertFalse(config.isExcluded(null));
+    }
+
+    @Test
+    public void whenDocumentationEndpointUriIsNotADocumentationUri() {
+        assertTrue(config.isExcluded("/pets"));
+    }
+
+    @Test
+    public void whenDocumentationEndpointUriIsADocumentationUri() {
+        assertTrue(config.isExcluded("/api-docs/pets"));
+    }
+
+    @Test
+    public void whenDocumentationEndpointUriIsADocumentationUriThatDoesNotExist() {
+        assertFalse(config.isExcluded("/api-docs/does-not-exist"));
+    }
+}
