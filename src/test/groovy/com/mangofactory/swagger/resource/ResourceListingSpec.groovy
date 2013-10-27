b28ced0851faaad138e4bd4eb9934e6d63c8cba7
Adrian Kelly <akelly@dius.com.au>
2013-10-27 23:12:21 +1100

    Cleaned conflicting dependencies for swagger upgrade

---
+++ src/test/groovy/com/mangofactory/swagger/resource/ResourceListingSpec.groovy
@@ -0,0 +1,11 @@
+package com.mangofactory.swagger.resource
+
+import spock.lang.Specification
+
+class ResourceListingSpec extends Specification {
+
+    def "test"(){
+        expect:
+        true
+    }
+}
