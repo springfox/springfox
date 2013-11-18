88c37546bf387b070e91571b590b02bdfe85d73b
Adrian Kelly <akelly@dius.com.au>
2013-11-18 23:46:25 +1100

    Remove @Component - client spring config should configure this bean if its required

--- src/main/java/com/mangofactory/swagger/configuration/JacksonScalaSupport.java
+++ src/main/java/com/mangofactory/swagger/configuration/JacksonScalaSupport.java
@@ -4,24 +4,26 @@ import com.fasterxml.jackson.module.scala.DefaultScalaModule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
 
-@Component
 @SuppressWarnings({"SpringJavaAutowiringInspection"})
 public class JacksonScalaSupport {
+
+   private Boolean registerScalaModule = false;
    private RequestMappingHandlerAdapter requestMappingHandlerAdapter;
 
    @PostConstruct
    public void init() {
-      List<HttpMessageConverter<?>> messageConverters = requestMappingHandlerAdapter.getMessageConverters();
-      for (HttpMessageConverter<?> messageConverter : messageConverters) {
-         if (messageConverter instanceof MappingJackson2HttpMessageConverter) {
-            MappingJackson2HttpMessageConverter m = (MappingJackson2HttpMessageConverter) messageConverter;
-            m.getObjectMapper().registerModule(new DefaultScalaModule());
+      if (registerScalaModule) {
+         List<HttpMessageConverter<?>> messageConverters = requestMappingHandlerAdapter.getMessageConverters();
+         for (HttpMessageConverter<?> messageConverter : messageConverters) {
+            if (messageConverter instanceof MappingJackson2HttpMessageConverter) {
+               MappingJackson2HttpMessageConverter m = (MappingJackson2HttpMessageConverter) messageConverter;
+               m.getObjectMapper().registerModule(new DefaultScalaModule());
+            }
          }
       }
    }
@@ -29,5 +31,13 @@ public class JacksonScalaSupport {
    @Autowired
    public void setRequestMappingHandlerAdapter(RequestMappingHandlerAdapter requestMappingHandlerAdapter) {
       this.requestMappingHandlerAdapter = requestMappingHandlerAdapter;
+   }
+
+   public Boolean getRegisterScalaModule() {
+      return registerScalaModule;
+   }
+
+   public void setRegisterScalaModule(Boolean registerScalaModule) {
+      this.registerScalaModule = registerScalaModule;
    }
 }
