b88b1238cb7b779569d9cc65870b2740c1ce8052
Adrian Kelly <akelly@dius.com.au>
2013-11-19 20:24:38 +1100

    Testing JacksonScalaSupport

---
+++ src/test/groovy/com/mangofactory/swagger/configuration/JacksonScalaSupportSpec.groovy
@@ -0,0 +1,34 @@
+package com.mangofactory.swagger.configuration
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter
+import spock.lang.Specification
+
+class JacksonScalaSupportSpec extends Specification {
+
+   def "should add scala module based on boolean flag"() {
+    given:
+      RequestMappingHandlerAdapter requestMappingHandlerAdapter = Mock()
+      ObjectMapper objectMapper = Mock()
+      MappingJackson2HttpMessageConverter httpMessageConverter = Mock()
+      httpMessageConverter.getObjectMapper() >> objectMapper
+      requestMappingHandlerAdapter.getMessageConverters() >> [httpMessageConverter]
+    when:
+      JacksonScalaSupport jacksonScalaSupport = new JacksonScalaSupport(
+              requestMappingHandlerAdapter: requestMappingHandlerAdapter,
+              registerScalaModule: true
+      )
+      jacksonScalaSupport.init()
+    then:
+      1 * objectMapper.registerModule(_)
+
+    when:
+      jacksonScalaSupport = new JacksonScalaSupport(
+              requestMappingHandlerAdapter: requestMappingHandlerAdapter,
+              registerScalaModule: false)
+      jacksonScalaSupport.init()
+    then:
+      0 * objectMapper.registerModule(_)
+   }
+}
