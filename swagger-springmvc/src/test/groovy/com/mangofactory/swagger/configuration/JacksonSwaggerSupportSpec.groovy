5dd4880a32db956b31c599a70db678085f0f35ce
Adrian Kelly <adrianbkelly@gmail.com>
2014-06-10 13:01:28 +1000

    Testing JacksonSwaggerSupport

--- swagger-springmvc/src/test/groovy/com/mangofactory/swagger/configuration/JacksonSwaggerSupportSpec.groovy
+++ swagger-springmvc/src/test/groovy/com/mangofactory/swagger/configuration/JacksonSwaggerSupportSpec.groovy
@@ -1,21 +1,36 @@
 package com.mangofactory.swagger.configuration
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import spock.lang.Ignore
+import com.mangofactory.swagger.models.DefaultModelPropertiesProvider
+import org.springframework.context.ApplicationContext
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter
 import spock.lang.Specification
 
 class JacksonSwaggerSupportSpec extends Specification {
 
-  @Ignore
-  def "should add scala module based on boolean flag"() {
+  def "Should register swagger module and set object mapper on DefaultModelPropertiesProvider"() {
     given:
       JacksonSwaggerSupport jacksonSwaggerSupport = new JacksonSwaggerSupport()
       ObjectMapper objectMapper = Mock()
-      jacksonSwaggerSupport.springsMessageConverterObjectMapper = objectMapper
+      MappingJackson2HttpMessageConverter jacksonMessageConverter = Mock()
+      jacksonMessageConverter.getObjectMapper() >> objectMapper
+
+      ApplicationContext applicationContext = Mock()
+
+      DefaultModelPropertiesProvider defaultModelPropertiesProvider = Mock()
+      applicationContext.getBeansOfType(_) >> ['beanName': defaultModelPropertiesProvider]
+
+      RequestMappingHandlerAdapter requestMappingHandlerAdapter = Mock()
+      requestMappingHandlerAdapter.getMessageConverters() >> [jacksonMessageConverter]
+
+      jacksonSwaggerSupport.requestMappingHandlerAdapter = requestMappingHandlerAdapter
+      jacksonSwaggerSupport.applicationContext = applicationContext
 
     when:
       jacksonSwaggerSupport.setup()
     then:
       1 * objectMapper.registerModule(_)
+      1 * defaultModelPropertiesProvider.setObjectMapper(objectMapper)
   }
 }
