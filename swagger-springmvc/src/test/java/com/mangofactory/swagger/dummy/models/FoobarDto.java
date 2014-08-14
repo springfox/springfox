f69fce21dd3edd31e57ccb273d1566c1e22cb552
Dilip Krishnan <d_krishnan@containerstore.com>
2014-08-14 09:44:54 -0500

    Updated the test to reflect JsonProperty usecase in the constructor

--- swagger-springmvc/src/test/java/com/mangofactory/swagger/dummy/models/FoobarDto.java
+++ swagger-springmvc/src/test/java/com/mangofactory/swagger/dummy/models/FoobarDto.java
@@ -1,10 +1,8 @@
 package com.mangofactory.swagger.dummy.models;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonValue;
 
 public class FoobarDto {
 
@@ -27,50 +25,19 @@ public class FoobarDto {
 
   @JsonProperty("foobar")
   public String getFoobarCode() {
-    return foobar == null ? null : foobar.getDisplayName();
+    return foobar == null ? null : foobar.name();
   }
 
   public Foobar getFoobar() {
     return foobar;
   }
 
-  public FoobarDto(Foobar foobar) {
-    this.foobar = foobar;
+  public FoobarDto(@JsonProperty("foobar") String foobar) {
+    this.foobar = Foobar.valueOf(foobar);
   }
 
   public enum Foobar {
-    FOO(0, "Foo"),
-    BAR(1, "Bar")
-    ;
-    private final int value;
-
-    @JsonCreator
-    public static Foobar lookup(Integer value) {
-      if(value == null) {
-        return null;
-      }
-      for (Foobar obj : values()) {
-        if (obj.value == value) {
-          return obj;
-        }
-      }
-      return null;
-    }
-
-    private final String displayName;
-
-    @JsonValue
-    public int getValue() {
-      return value;
-    }
-
-    public String getDisplayName() {
-      return displayName;
-    }
-
-    private Foobar(int value, String displayName) {
-      this.value = value;
-      this.displayName = displayName;
-    }
+    FOO,
+    BAR
   }
 }
