f4b8ce5613c9858c18c29a967534d3bc6b2b0ff3
fgaule <federico.gaule@gmail.com>
2014-08-15 16:38:44 -0300

    Fine-grained tests to check if #409 was responsible of #413

--- swagger-springmvc/src/test/groovy/com/mangofactory/swagger/mixins/ModelProviderSupport.groovy
+++ swagger-springmvc/src/test/groovy/com/mangofactory/swagger/mixins/ModelProviderSupport.groovy
@@ -1,6 +1,7 @@
 package com.mangofactory.swagger.mixins
 import com.fasterxml.classmate.TypeResolver
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import com.mangofactory.swagger.models.DefaultModelProvider
 import com.mangofactory.swagger.models.ModelDependencyProvider
 import com.mangofactory.swagger.models.ModelProvider
@@ -21,6 +22,28 @@ class ModelProviderSupport {
     def fields = new FieldProvider(typeResolver)
 
     def objectMapper = new ObjectMapper()
+    def namingStrategy = new ObjectMapperBeanPropertyNamingStrategy(objectMapper)
+
+    def beanModelPropertyProvider = new BeanModelPropertyProvider(new AccessorsProvider(typeResolver), typeResolver,
+            alternateTypeProvider, namingStrategy)
+    def fieldModelPropertyProvider = new FieldModelPropertyProvider(fields, alternateTypeProvider)
+    def constructorModelPropertyProvider = new ConstructorModelPropertyProvider(fields, alternateTypeProvider)
+
+    def modelPropertiesProvider = new DefaultModelPropertiesProvider(beanModelPropertyProvider,
+            fieldModelPropertyProvider, constructorModelPropertyProvider)
+
+    modelPropertiesProvider.objectMapper = objectMapper
+    def modelDependenciesProvider = new ModelDependencyProvider(typeResolver, alternateTypeProvider, modelPropertiesProvider)
+    new DefaultModelProvider(typeResolver, alternateTypeProvider, modelPropertiesProvider, modelDependenciesProvider)
+  }
+
+  ModelProvider modelProviderWithSnakeCaseNamingStrategy(TypeResolver typeResolver = new TypeResolver(),
+                              AlternateTypeProvider alternateTypeProvider = new AlternateTypeProvider()) {
+
+    def fields = new FieldProvider(typeResolver)
+
+    def objectMapper = new ObjectMapper()
+    objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.CAMEL_CASE_TO_LOWER_CASE_WITH_UNDERSCORES)
     def namingStrategy = new ObjectMapperBeanPropertyNamingStrategy(objectMapper)
 
     def beanModelPropertyProvider = new BeanModelPropertyProvider(new AccessorsProvider(typeResolver), typeResolver,
