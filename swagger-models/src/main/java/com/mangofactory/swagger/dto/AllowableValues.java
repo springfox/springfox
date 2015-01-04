27646850207e7041f33dc1259ad293463d1b64db
Dilip Krishnan <d_krishnan@containerstore.com>
2015-01-04 09:32:28 -0600

    Removed unused class

--- swagger-models/src/main/java/com/mangofactory/swagger/dto/AllowableValues.java
+++ swagger-models/src/main/java/com/mangofactory/swagger/dto/AllowableValues.java
@@ -1,4 +1,9 @@
 package com.mangofactory.swagger.dto;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = AllowableListValues.class),
+        @JsonSubTypes.Type(value = AllowableRangeValues.class) })
 public interface AllowableValues {
 }
