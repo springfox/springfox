6ecbe4a5e8ca04d0ed248fb15b6c84fcfbc2d831
Adrian Kelly <adrianbkelly@gmail.com>
2015-03-16 21:02:45 +1100

    Fixed buildSrc package issue

---
+++ buildSrc/src/test/groovy/springdox/gradlebuild/SemanticVersionTest.groovy
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package springdox.gradlebuild
+
+import spock.lang.Specification
+
+class SemanticVersionTest extends Specification {
+
+  def "should calculate the next version number"() {
+    def semVersion = new SemanticVersion(0, 0, 0)
+    def next = semVersion.next(releaseType)
+    expect:
+      next.asText() == expected
+    where:
+      releaseType       | expected
+      ReleaseType.MAJOR | '1.0.0'
+      ReleaseType.MINOR | '0.1.0'
+      ReleaseType.PATCH | '0.0.1'
+      ReleaseType.PATCH | '0.0.1'
+  }
+
+  def "should load from a prop file"() {
+    File tempDir = new File('buildSrc/build/files')
+    tempDir.mkdirs()
+    File propFile = new File("${tempDir.absolutePath}/p.properties")
+    propFile.createNewFile()
+    propFile << '''
+major=1
+minor=1
+patch=1
+'''
+    expect:
+      def semanticVersion = SemanticVersion.get(propFile)
+      semanticVersion.major == 1
+      semanticVersion.minor == 1
+      semanticVersion.patch == 1
+  }
+}
