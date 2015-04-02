296ab8182afd53d0d2429fd77e6b61054406de67
Dilip Krishnan <d_krishnan@containerstore.com>
2015-04-02 10:28:37 -0500

    Formatting fixes

--- springfox-swagger/src/test/java/springfox/documentation/swagger/configuration/CustomJavaPluginConfig.java
+++ springfox-swagger/src/test/java/springfox/documentation/swagger/configuration/CustomJavaPluginConfig.java
@@ -39,42 +39,42 @@ import static springfox.documentation.spi.DocumentationType.*;
 @ComponentScan("springfox.documentation.spring.web.dummy") //Scan some controllers
 public class CustomJavaPluginConfig {
 
-    /**
-     * Every SwaggerSpringMvcPlugin bean is picked up by the swagger-mvc framework - allowing for multiple
-     * swagger groups i.e. same code base multiple swagger resource listings
-     */
-    @Bean
-    public Docket customImplementation() {
-        return new Docket(SWAGGER_12)
-                .groupName("customPlugin")
-                .select()
-                .paths(PathSelectors.regex(".*pet.*"))
-                .build();
-    }
+  /**
+   * Every SwaggerSpringMvcPlugin bean is picked up by the swagger-mvc framework - allowing for multiple
+   * swagger groups i.e. same code base multiple swagger resource listings
+   */
+  @Bean
+  public Docket customImplementation() {
+    return new Docket(SWAGGER_12)
+        .groupName("customPlugin")
+        .select()
+          .paths(PathSelectors.regex(".*pet.*"))
+          .build();
+  }
 
-    @Bean
-    public Docket secondCustomImplementation() {
-        return new Docket(SWAGGER_12)
-                .groupName("secondCustomPlugin")
-                .apiInfo(apiInfo())
-                .select()
-                .paths(PathSelectors.regex("/feature.*"))
-                .build();
-    }
+  @Bean
+  public Docket secondCustomImplementation() {
+    return new Docket(SWAGGER_12)
+        .groupName("secondCustomPlugin")
+        .apiInfo(apiInfo())
+        .select()
+          .paths(PathSelectors.regex("/feature.*"))
+          .build();
+  }
 
-    @Bean
-    public ObjectMapper objectMapper() {
-        return new ObjectMapper();
-    }
+  @Bean
+  public ObjectMapper objectMapper() {
+    return new ObjectMapper();
+  }
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("My Apps API Title")
-                .description("My Apps API Description")
-                .termsOfServiceUrl("My Apps API terms of service")
-                .contact("My Apps API Contact Email")
-                .license("My Apps API Licence Type")
-                .licenseUrl("My Apps API License URL")
-                .build();
-    }
+  private ApiInfo apiInfo() {
+    return new ApiInfoBuilder()
+        .title("My Apps API Title")
+        .description("My Apps API Description")
+        .termsOfServiceUrl("My Apps API terms of service")
+        .contact("My Apps API Contact Email")
+        .license("My Apps API Licence Type")
+        .licenseUrl("My Apps API License URL")
+        .build();
+  }
 }
