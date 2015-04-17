c6c3eb1e458435e6749de66fe2b5d6c02562ad0b
Adrian Kelly <adrianbkelly@gmail.com>
2015-04-18 00:35:52 +1000

    Adds a basic starter example to the getting started guide

--- springfox-spring-config/src/main/java/springfox/springconfig/Swagger2SpringBoot.java
+++ springfox-spring-config/src/main/java/springfox/springconfig/Swagger2SpringBoot.java
@@ -3,15 +3,26 @@ package springfox.springconfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import springfox.petstore.controller.PetController;
 
 @SpringBootApplication
-@EnableSwagger2 //1
-@ComponentScan(basePackageClasses = {PetController.class})
+@EnableSwagger2   //<1>
+@ComponentScan(basePackageClasses = {PetController.class})    //<2>
 public class Swagger2SpringBoot {
+
   public static void main(String[] args) {
     ApplicationContext ctx = SpringApplication.run(Swagger2SpringBoot.class, args);
+  }
+
+  @Bean
+  public Docket petApi() {
+    return new Docket(DocumentationType.SWAGGER_2)    //<3>
+            .select()   //<4>
+            .build();   //<5>
   }
 }
