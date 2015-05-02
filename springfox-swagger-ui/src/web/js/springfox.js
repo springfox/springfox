294a41301fbdcc05e428f6de0acd323601397a0f
Dilip Krishnan <d_krishnan@containerstore.com>
2015-05-01 19:04:20 -0500

    Updated the script to work on reload
    fixes #713

--- springfox-swagger-ui/src/web/js/springfox.js
+++ springfox-swagger-ui/src/web/js/springfox.js
@@ -1,33 +1,34 @@
-$(function () {
+$(function() {
+    function baseUrl() {
+        var urlMatches =  /(.*)\/swagger-ui.html.*/.exec(window.location.href);
+        return urlMatches[1];
+    }
 
-    var relativeLocation = baseUrl();
-
-    $('#input_baseUrl').hide();
-    var swaggerDropdown = $('#select_baseUrl');
-
-    function refresh() {
+    $('#select_baseUrl').change(function () {
         window.swaggerUi.headerView.trigger('update-swagger-ui', {
             url: swaggerDropdown.val()
         });
-    }
-
-    function baseUrl() {
-        var parts = window.location.href.split('/');
-        //Walk up the url
-        parts.pop();
-        return parts.join('/');
-    }
-
-    $.getJSON(relativeLocation + "/swagger-resources", function (data) {
-        var items = [];
-        swaggerDropdown.empty();
-        $.each(data, function (i, resource) {
-            var option = $('<option></option>').attr("value", relativeLocation + resource.location).text(resource.name + " (" + resource.location + ")");
-            swaggerDropdown.append(option);
-        });
-        refresh();
     });
-    swaggerDropdown.change(refresh);
+
+    $(document).ready(function() {
+        var relativeLocation = baseUrl();
+
+        $('#input_baseUrl').hide();
+
+        $.getJSON(relativeLocation + "/swagger-resources", function(data) {
+
+            var $urlDropdown = $('#select_baseUrl');
+            $urlDropdown.empty();
+            $.each(data, function(i, resource) {
+                var option = $('<option></option>')
+                        .attr("value", relativeLocation + resource.location)
+                        .text(resource.name + " (" + resource.location + ")");
+                $urlDropdown.append(option);
+            });
+            $urlDropdown.change();
+        });
+    });
+
 });
 
 
