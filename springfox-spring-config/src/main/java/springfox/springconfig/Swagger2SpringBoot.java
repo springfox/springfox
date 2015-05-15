441b4608d7bd541fdf9c96468cfd95a467796ed9
Dilip Krishnan <d_krishnan@containerstore.com>
2015-05-14 21:27:30 -0500

    Added more documentation to the asciidoc template class
    related to #531

--- springfox-spring-config/src/main/java/springfox/springconfig/Swagger2SpringBoot.java
+++ springfox-spring-config/src/main/java/springfox/springconfig/Swagger2SpringBoot.java
@@ -1,28 +1,94 @@
 package springfox.springconfig;
 
+import com.fasterxml.classmate.TypeResolver;
+import org.joda.time.LocalDate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.async.DeferredResult;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.schema.WildcardType;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import springfox.petstore.controller.PetController;
 
+import java.util.List;
+
+import static com.google.common.collect.Lists.*;
+import static springfox.documentation.schema.AlternateTypeRules.*;
+
 @SpringBootApplication
-@EnableSwagger2   //<1>
-@ComponentScan(basePackageClasses = {PetController.class})    //<2>
+@EnableSwagger2                                                              //<1>
+@ComponentScan(basePackageClasses = {
+    PetController.class
+})                                                                           //<2>
 public class Swagger2SpringBoot {
 
   public static void main(String[] args) {
     ApplicationContext ctx = SpringApplication.run(Swagger2SpringBoot.class, args);
   }
 
+
+
   @Bean
   public Docket petApi() {
-    return new Docket(DocumentationType.SWAGGER_2)    //<3>
-            .select()   //<4>
-            .build();   //<5>
+    return new Docket(DocumentationType.SWAGGER_2)                           //<3>
+            .select()                                                        //<4>
+              .apis(RequestHandlerSelectors.any())                           //<5>
+              .paths(PathSelectors.any())                                    //<6>
+              .build()                                                       //<7>
+            .pathMapping("/")                                                //<8>
+            .directModelSubstitute(LocalDate.class,
+                String.class)                                                //<9>
+            .genericModelSubstitutes(ResponseEntity.class)                   //<10>
+            .alternateTypeRules(
+                newRule(typeResolver.resolve(DeferredResult.class,
+                        typeResolver.resolve(ResponseEntity.class, WildcardType.class)),
+                  typeResolver.resolve(WildcardType.class)))                //<10a>
+            .useDefaultResponseMessages(false)                               //<11>
+            .globalResponseMessage(RequestMethod.GET,                        //<12>
+                newArrayList(new ResponseMessageBuilder()
+                    .code(500)
+                    .message("500 message")
+                    .responseModel(new ModelRef("Error"))                    //<13>
+                    .build()))
+            .securitySchemes(newArrayList(apiKey()))                         //<14>
+            .securityContexts(newArrayList(securityContext()))               //<15>
+        ;
+  }
+
+  @Autowired
+  private TypeResolver typeResolver;
+
+  private ApiKey apiKey() {
+    return new ApiKey("mykey", "api_key", "header");                         //<16>
+  }
+
+  private SecurityContext securityContext() {
+    return SecurityContext.builder()
+        .securityReferences(defaultAuth())
+        .forPaths(PathSelectors.regex("/anyPath.*"))                         //<17>
+        .build();
+  }
+
+  List<SecurityReference> defaultAuth() {
+    AuthorizationScope authorizationScope
+        = new AuthorizationScope("global", "accessEverything");
+    AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+    authorizationScopes[0] = authorizationScope;
+    newArrayList(new SecurityReference("mykey", authorizationScopes));       //<18>
   }
 }
