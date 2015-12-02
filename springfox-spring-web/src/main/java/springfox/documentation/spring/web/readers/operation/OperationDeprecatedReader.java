8f4121bed30a57a77a497736cf846207c277db7d
Dilip Krishnan <dilipkrish@users.noreply.github.com>
2015-12-02 06:15:14 -0600

    Pass the class name rather than the Class itself
    Since  is a jdk annotation it is probably not available
    in the classloader that the HandlerMethod#getMethodAnnotation uses to
    search for annotations on the handler method
    
    fixes #1055

--- springfox-spring-web/src/main/java/springfox/documentation/spring/web/readers/operation/OperationDeprecatedReader.java
+++ springfox-spring-web/src/main/java/springfox/documentation/spring/web/readers/operation/OperationDeprecatedReader.java
@@ -26,12 +26,15 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.OperationBuilderPlugin;
 import springfox.documentation.spi.service.contexts.OperationContext;
 
+import static org.springframework.core.annotation.AnnotatedElementUtils.*;
+
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class OperationDeprecatedReader implements OperationBuilderPlugin {
   @Override
   public void apply(OperationContext context) {
-    boolean isDeprecated = context.getHandlerMethod().getMethodAnnotation(Deprecated.class) != null;
+    boolean isDeprecated = isAnnotated(context.getHandlerMethod().getMethod(),
+        Deprecated.class.getName());
     context.operationBuilder().deprecated(String.valueOf(isDeprecated));
   }
 
