f653bb833ed858982bf5e59c05b4ea8ff44498c5
Dilip Krishnan <d_krishnan@isdv157182l.containerstore.com>
2016-02-04 13:22:06 -0600

    Fixed the incorrect check when updating parameter defaults
    Also updated the corresponding tests.
    
    closes #1165

--- springfox-swagger-common/src/main/java/springfox/documentation/swagger/readers/parameter/ParameterDefaultReader.java
+++ springfox-swagger-common/src/main/java/springfox/documentation/swagger/readers/parameter/ParameterDefaultReader.java
@@ -23,7 +23,6 @@ import io.swagger.annotations.ApiParam;
 import org.springframework.core.MethodParameter;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.ValueConstants;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.ParameterBuilderPlugin;
 import springfox.documentation.spi.service.contexts.ParameterContext;
@@ -31,7 +30,6 @@ import springfox.documentation.swagger.common.SwaggerPluginSupport;
 
 import java.lang.annotation.Annotation;
 
-import static com.google.common.base.Strings.*;
 import static org.springframework.util.StringUtils.*;
 
 @Component("swaggerParameterDefaultReader")
@@ -41,10 +39,7 @@ public class ParameterDefaultReader implements ParameterBuilderPlugin {
   public void apply(ParameterContext context) {
     MethodParameter methodParameter = context.methodParameter();
     String defaultValue = findAnnotatedDefaultValue(methodParameter);
-    boolean isSkip = ValueConstants.DEFAULT_NONE.equals(defaultValue);
-    if (!isSkip) {
-      context.parameterBuilder().defaultValue(nullToEmpty(defaultValue));
-    }
+    context.parameterBuilder().defaultValue(defaultValue);
   }
 
   @Override
