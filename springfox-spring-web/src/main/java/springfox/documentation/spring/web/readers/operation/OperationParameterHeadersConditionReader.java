0ba84498f7a7854919650d3d5e163b61a425bb1e
Ryan Scott <U0095868@U0095868-TPL-B.ten.thomsonreuters.com>
2016-04-29 11:00:41 -0500

    Added support for documenting headers in @RequestMapping

---
+++ springfox-spring-web/src/main/java/springfox/documentation/spring/web/readers/operation/OperationParameterHeadersConditionReader.java
@@ -0,0 +1,48 @@
+/*
+ *
+ *  Copyright 2016 the original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ */
+
+package springfox.documentation.spring.web.readers.operation;
+
+import com.fasterxml.classmate.TypeResolver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.mvc.condition.HeadersRequestCondition;
+import springfox.documentation.service.Parameter;
+import springfox.documentation.spi.service.contexts.OperationContext;
+
+import java.util.List;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE + 10)
+public class OperationParameterHeadersConditionReader extends AbstractOperationParameterRequestConditionReader {
+
+  @Autowired
+  public OperationParameterHeadersConditionReader(TypeResolver resolver) {
+    super(resolver);
+  }
+
+  @Override
+  public void apply(OperationContext context) {
+    HeadersRequestCondition headersCondition = context.getRequestMappingInfo().getHeadersCondition();
+    List<Parameter> parameters = getParameters(headersCondition.getExpressions(), "header");
+    context.operationBuilder().parameters(parameters);
+  }
+}
