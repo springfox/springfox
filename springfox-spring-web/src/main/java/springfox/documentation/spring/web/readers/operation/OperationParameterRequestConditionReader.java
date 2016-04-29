237f800dc657159679e615209b8b9cf3a3e3cc08
Ryan Scott <U0095868@U0095868-TPL-B.ten.thomsonreuters.com>
2016-04-29 10:59:26 -0500

    Refactored common code into abstract class

--- springfox-spring-web/src/main/java/springfox/documentation/spring/web/readers/operation/OperationParameterRequestConditionReader.java
+++ springfox-spring-web/src/main/java/springfox/documentation/spring/web/readers/operation/OperationParameterRequestConditionReader.java
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright 2015 the original author or authors.
+ *  Copyright 2016 the original author or authors.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -24,74 +24,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.mvc.condition.NameValueExpression;
 import org.springframework.web.servlet.mvc.condition.ParamsRequestCondition;
-import springfox.documentation.builders.ParameterBuilder;
-import springfox.documentation.schema.ModelRef;
-import springfox.documentation.service.AllowableListValues;
 import springfox.documentation.service.Parameter;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.OperationBuilderPlugin;
 import springfox.documentation.spi.service.contexts.OperationContext;
 
 import java.util.List;
 
-import static com.google.common.base.Strings.isNullOrEmpty;
-import static com.google.common.collect.Iterables.*;
-import static com.google.common.collect.Lists.*;
-import static springfox.documentation.builders.Parameters.*;
-
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE + 10)
-public class OperationParameterRequestConditionReader implements OperationBuilderPlugin {
-
-  private final TypeResolver resolver;
+public class OperationParameterRequestConditionReader extends AbstractOperationParameterRequestConditionReader {
 
   @Autowired
   public OperationParameterRequestConditionReader(TypeResolver resolver) {
-    this.resolver = resolver;
+    super(resolver);
   }
 
   @Override
   public void apply(OperationContext context) {
     ParamsRequestCondition paramsCondition = context.getRequestMappingInfo().getParamsCondition();
-    List<Parameter> parameters = newArrayList();
-    for (NameValueExpression<String> expression : paramsCondition.getExpressions()) {
-      if (skipParameter(parameters, expression)) {
-        continue;
-      }
-
-      String paramValue = expression.getValue();
-      AllowableListValues allowableValues = null;
-      if (!isNullOrEmpty(paramValue)) {
-        allowableValues = new AllowableListValues(newArrayList(paramValue), "string");
-      }
-      Parameter parameter = new ParameterBuilder()
-              .name(expression.getName())
-              .description(null)
-              .defaultValue(paramValue)
-              .required(true)
-              .allowMultiple(false)
-              .type(resolver.resolve(String.class))
-              .modelRef(new ModelRef("string"))
-              .allowableValues(allowableValues)
-              .parameterType("query")
-              .build();
-      parameters.add(parameter);
-    }
+    List<Parameter> parameters = getParameters(paramsCondition.getExpressions(), "query");
     context.operationBuilder().parameters(parameters);
-  }
-
-  private boolean skipParameter(List<Parameter> parameters, NameValueExpression<String> expression) {
-    return expression.isNegated() || parameterHandled(parameters, expression);
-  }
-
-  private boolean parameterHandled(List<Parameter> parameters, NameValueExpression<String> expression) {
-    return any(parameters, withName(expression.getName()));
-  }
-
-  @Override
-  public boolean supports(DocumentationType delimiter) {
-    return true;
   }
 }
