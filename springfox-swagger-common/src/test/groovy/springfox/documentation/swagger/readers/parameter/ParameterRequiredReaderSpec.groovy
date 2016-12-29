6dfaf3acb1af25b192a1f8d05f042adcc65a108a
dilipkrish <dilipkrish@users.noreply.github.com>
2016-12-28 20:11:21 -0600

    Fixed formatting of the test

--- springfox-swagger-common/src/test/groovy/springfox/documentation/swagger/readers/parameter/ParameterRequiredReaderSpec.groovy
+++ springfox-swagger-common/src/test/groovy/springfox/documentation/swagger/readers/parameter/ParameterRequiredReaderSpec.groovy
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright 2015 the original author or authors.
+ *  Copyright 2017 the original author or authors.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -49,17 +49,17 @@ class ParameterRequiredReaderSpec extends DocumentationContextSpec implements Ap
 
   def "parameters hidden using default reader"() {
     given:
-    def parameterContext = setupParameterContext(paramAnnotation)
+      def parameterContext = setupParameterContext(paramAnnotation)
     when:
-    def operationCommand = stubbedParamBuilder(paramAnnotation);
-    operationCommand.apply(parameterContext)
+      def operationCommand = stubbedParamBuilder(paramAnnotation);
+      operationCommand.apply(parameterContext)
     then:
-    parameterContext.parameterBuilder().build().isHidden() == expected
+      parameterContext.parameterBuilder().build().isHidden() == expected
     where:
-    paramAnnotation             | expected
-    apiParamWithHidden(false)   | false
-    apiParamWithHidden(true)    | true
-    null                        | false
+      paramAnnotation             | expected
+      apiParamWithHidden(false)   | false
+      apiParamWithHidden(true)    | true
+      null                        | false
   }
 
   def setupParameterContext(paramAnnotation) {
