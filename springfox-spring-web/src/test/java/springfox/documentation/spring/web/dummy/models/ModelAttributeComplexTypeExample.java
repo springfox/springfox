a35f0f904e30f7421d9f58702319776069d33958
dilipkrish <dilipkrish@users.noreply.github.com>
2017-02-14 21:43:23 -0600

    Reformatted code
    Related to #1603

--- springfox-spring-web/src/test/java/springfox/documentation/spring/web/dummy/models/ModelAttributeComplexTypeExample.java
+++ springfox-spring-web/src/test/java/springfox/documentation/spring/web/dummy/models/ModelAttributeComplexTypeExample.java
@@ -23,43 +23,43 @@ import java.util.List;
 
 public class ModelAttributeComplexTypeExample extends ModelAttributeExample {
 
-    private List<FancyPet> fancyPets;
+  private List<FancyPet> fancyPets;
 
-    private Category[] categories;
+  private Category[] categories;
 
-    private String[] modelAttributeProperty;
+  private String[] modelAttributeProperty;
 
-    private List<ModelAttributeComplexTypeExample> recursiveList;
+  private List<ModelAttributeComplexTypeExample> recursiveList;
 
-    public List<FancyPet> getFancyPets() {
-        return fancyPets;
-    }
+  public List<FancyPet> getFancyPets() {
+    return fancyPets;
+  }
 
-    public void setFancyPets(List<FancyPet> fancyPets) {
-        this.fancyPets = fancyPets;
-    }
+  public void setFancyPets(List<FancyPet> fancyPets) {
+    this.fancyPets = fancyPets;
+  }
 
-    public Category[] getCategories() {
-        return categories;
-    }
+  public Category[] getCategories() {
+    return categories;
+  }
 
-    public void setCategories(Category[] categories) {
-        this.categories = categories;
-    }
+  public void setCategories(Category[] categories) {
+    this.categories = categories;
+  }
 
-    public String[] getModelAttributeProperty() {
-        return modelAttributeProperty;
-    }
+  public String[] getModelAttributeProperty() {
+    return modelAttributeProperty;
+  }
 
-    public void setModelAttributeProperty(String[] modelAttributeProperty) {
-        this.modelAttributeProperty = modelAttributeProperty;
-    }
+  public void setModelAttributeProperty(String[] modelAttributeProperty) {
+    this.modelAttributeProperty = modelAttributeProperty;
+  }
 
-    public List<ModelAttributeComplexTypeExample> getRecursiveList() {
-        return recursiveList;
-    }
+  public List<ModelAttributeComplexTypeExample> getRecursiveList() {
+    return recursiveList;
+  }
 
-    public void setRecursiveList(List<ModelAttributeComplexTypeExample> recursiveList) {
-        this.recursiveList = recursiveList;
-    }
+  public void setRecursiveList(List<ModelAttributeComplexTypeExample> recursiveList) {
+    this.recursiveList = recursiveList;
+  }
 }
