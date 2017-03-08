189004ef57f32559970b2a3730dd69058a04529e
dilipkrish <dilipkrish@users.noreply.github.com>
2017-03-08 13:18:06 -0600

    Added ability to override the base url
    Since springfox relies on the position of swagger-ui.html in the URL, when that is not accurate in certain cases (#1615) or may be missing (#1707) we provide a prompt so the user can update the base url manually. This makes the UI useable even in cases where the api metadata is not available at the expected location.
    
    fixes #1615
    fixes #1707

--- springfox-swagger-ui/src/web/js/springfox.js
+++ springfox-swagger-ui/src/web/js/springfox.js
@@ -1,24 +1,72 @@
 $(function() {
+  var baseUrl;
   var springfox = {
-    "baseUrl": function() {
-      var urlMatches = /(.*)\/swagger-ui.html.*/.exec(window.location.href);
-      return urlMatches[1];
+    "getBaseUrl": function() {
+      if (baseUrl === undefined) {
+        var urlMatches = /(.*)\/swagger-ui.html.*/.exec(window.location.href);
+        baseUrl = urlMatches[1];
+      }
+      return baseUrl;
     },
-    "securityConfig": function(cb) {
-      $.getJSON(this.baseUrl() + "/swagger-resources/configuration/security", function(data) {
-        cb(data);
+    "setBaseUrl": function(override) {
+      baseUrl = override;
+    },
+    "securityConfig": function(onSuccess, onError) {
+      $.ajax({
+        url: this.getBaseUrl() + "/swagger-resources/configuration/security",
+        dataType: 'json',
+        success: onSuccess,
+        error: onError
       });
     },
-    "uiConfig": function(cb) {
-      $.getJSON(this.baseUrl() + "/swagger-resources/configuration/ui", function(data) {
-        cb(data);
+    "uiConfig": function(onSuccess, onError) {
+      $.ajax({
+        url: this.getBaseUrl() + "/swagger-resources/configuration/ui",
+        dataType: 'json',
+        success: onSuccess,
+        error: onError
       });
     }
   };
   window.springfox = springfox;
-  window.oAuthRedirectUrl = springfox.baseUrl() + '/webjars/springfox-swagger-ui/o2c.html';
+  window.oAuthRedirectUrl = springfox.getBaseUrl() + '/webjars/springfox-swagger-ui/o2c.html';
 
-  window.springfox.uiConfig(function(data) {
+  window.springfox.uiConfig(onSuccess, onError);
+
+  $('#select_baseUrl').change(function() {
+    window.swaggerUi.headerView.trigger('update-swagger-ui', {
+      url: $('#select_baseUrl').val()
+    });
+  });
+
+  function maybePrefix(location, withRelativePath) {
+    var pat = /^https?:\/\//i;
+    if (pat.test(location)) {
+      return location;
+    }
+    return withRelativePath + location;
+  }
+
+  function initializeBaseUrl() {
+    var relativeLocation = springfox.getBaseUrl();
+
+    $('#input_baseUrl').hide();
+
+    $.getJSON(relativeLocation + "/swagger-resources", function(data) {
+
+      var $urlDropdown = $('#select_baseUrl');
+      $urlDropdown.empty();
+      $.each(data, function(i, resource) {
+        var option = $('<option></option>')
+            .attr("value", maybePrefix(resource.location, relativeLocation))
+            .text(resource.name + " (" + resource.location + ")");
+        $urlDropdown.append(option);
+      });
+      $urlDropdown.change();
+    });
+  }
+
+  function onSuccess(data) {
     window.swaggerUi = new SwaggerUi({
       dom_id: "swagger-ui-container",
       validatorUrl: data.validatorUrl,
@@ -82,40 +130,16 @@ $(function() {
         }
       });
     }
-  });
-
-  $('#select_baseUrl').change(function() {
-    window.swaggerUi.headerView.trigger('update-swagger-ui', {
-      url: $('#select_baseUrl').val()
-    });
-  });
-
-  function maybePrefix(location, withRelativePath) {
-    var pat = /^https?:\/\//i;
-    if (pat.test(location)) {
-      return location;
-    }
-    return withRelativePath + location;
   }
 
-  function initializeBaseUrl() {
-    var relativeLocation = springfox.baseUrl();
-
-    $('#input_baseUrl').hide();
-
-    $.getJSON(relativeLocation + "/swagger-resources", function(data) {
-
-      var $urlDropdown = $('#select_baseUrl');
-      $urlDropdown.empty();
-      $.each(data, function(i, resource) {
-        var option = $('<option></option>')
-            .attr("value", maybePrefix(resource.location, relativeLocation))
-            .text(resource.name + " (" + resource.location + ")");
-        $urlDropdown.append(option);
-      });
-      $urlDropdown.change();
-    });
-
+  function onError() {
+    baseUrl = prompt(
+        "Unable to infer base url. This is common when using dynamic servlet registration or when" +
+        " the API is behind an API Gateway. The base url is the root of where" +
+        " all the swagger resources are served. For e.g. if the api is available at http://example.org/api/v2/api-docs" +
+        " then the base url is http://example.org/api/. Please enter the location manually: ",
+        window.location.href);
+    window.springfox.uiConfig(onSuccess, onError);
   }
 
 });
