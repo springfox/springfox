3a28602c48cbda956df5c87a59ee8a20fbb2d446
yuri <yuri@billforward.net>
2017-08-04 11:31:57 +0100

    Setting _default in StringProperty to match new defaultValue from ModelProperty

--- springfox-swagger2/src/main/java/springfox/documentation/swagger2/mappers/ModelMapper.java
+++ springfox-swagger2/src/main/java/springfox/documentation/swagger2/mappers/ModelMapper.java
@@ -170,6 +170,7 @@ public abstract class ModelMapper {
       if (source.getPattern() != null) {
         stringProperty.setPattern(source.getPattern());
       }
+      property.setDefault(source.getDefaultValue());
     }
 
     if (property != null) {
