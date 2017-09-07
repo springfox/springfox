30c08e69f52cce4e333eab1f56c72fc7e7a380ed
Dilip Krishnan <D_KRISHNAN@containerstore.com>
2017-09-06 22:28:34 -0500

    Created an empty shell based on swagger 2.0
    (2022)

---
+++ springfox-oas/src/main/java/springfox/documentation/oas/web/OasController.java
@@ -0,0 +1,109 @@
+/*
+ *
+ *  Copyright 2017-2018 the original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ */
+
+package springfox.documentation.oas.web;
+
+import com.google.common.base.Optional;
+import io.swagger.oas.models.OpenAPI;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.util.UriComponents;
+import springfox.documentation.annotations.ApiIgnore;
+import springfox.documentation.oas.mappers.ServiceModelToOasMapper;
+import springfox.documentation.service.Documentation;
+import springfox.documentation.spring.web.DocumentationCache;
+import springfox.documentation.spring.web.PropertySourcedMapping;
+import springfox.documentation.spring.web.json.Json;
+import springfox.documentation.spring.web.json.JsonSerializer;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.springframework.util.MimeTypeUtils.*;
+
+@Controller
+@ApiIgnore
+public class OasController {
+
+  public static final String DEFAULT_URL = "/v2/api-docs";
+  private static final String HAL_MEDIA_TYPE = "application/hal+json";
+
+  private final String hostNameOverride;
+  private final DocumentationCache documentationCache;
+  private final ServiceModelToOasMapper mapper;
+  private final JsonSerializer jsonSerializer;
+
+  @Autowired
+  public OasController(
+      Environment environment,
+      DocumentationCache documentationCache,
+      ServiceModelToOasMapper mapper,
+      JsonSerializer jsonSerializer) {
+
+    this.hostNameOverride = environment.getProperty("springfox.documentation.swagger.v2.host", "DEFAULT");
+    this.documentationCache = documentationCache;
+    this.mapper = mapper;
+    this.jsonSerializer = jsonSerializer;
+  }
+
+  @RequestMapping(
+      value = DEFAULT_URL,
+      method = RequestMethod.GET,
+      produces = { APPLICATION_JSON_VALUE, HAL_MEDIA_TYPE })
+  @PropertySourcedMapping(
+      value = "${springfox.documentation.oas.path}",
+      propertyKey = "springfox.documentation.oas.path")
+  @ResponseBody
+  public ResponseEntity<Json> getDocumentation(
+      @RequestParam(value = "group", required = false) String swaggerGroup,
+      HttpServletRequest servletRequest) {
+
+    String groupName = Optional.fromNullable(swaggerGroup).or(Docket.DEFAULT_GROUP_NAME);
+    Documentation documentation = documentationCache.documentationByGroup(groupName);
+    if (documentation == null) {
+      return new ResponseEntity<Json>(HttpStatus.NOT_FOUND);
+    }
+    OpenAPI oas = mapper.mapDocumentation(documentation);
+//    UriComponents uriComponents = componentsFrom(servletRequest, oas.getBasePath());
+//    oas.basePath(Strings.isNullOrEmpty(uriComponents.getPath()) ? "/" : uriComponents.getPath());
+//    if (isNullOrEmpty(oas.getHost())) {
+//      oas.host(hostName(uriComponents));
+//    }
+    return new ResponseEntity<Json>(jsonSerializer.toJson(oas), HttpStatus.OK);
+  }
+
+  private String hostName(UriComponents uriComponents) {
+    if ("DEFAULT".equals(hostNameOverride)) {
+      String host = uriComponents.getHost();
+      int port = uriComponents.getPort();
+      if (port > -1) {
+        return String.format("%s:%d", host, port);
+      }
+      return host;
+    }
+    return hostNameOverride;
+  }
+}
