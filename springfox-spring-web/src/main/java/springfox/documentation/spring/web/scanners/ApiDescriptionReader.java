1adb232d3e84f301f9341e09ea7cd3479fab41ef
limingcai <50043243@qq.com>
2017-10-10 16:12:56 +0800

    add try/catch error and log for easy find Cycles in Java classes cause infinite loop in ModelAttributeParameterExpander StackOverflowError

--- springfox-spring-web/src/main/java/springfox/documentation/spring/web/scanners/ApiDescriptionReader.java
+++ springfox-spring-web/src/main/java/springfox/documentation/spring/web/scanners/ApiDescriptionReader.java
@@ -41,55 +41,56 @@ import static com.google.common.collect.Ordering.*;
 
 @Component
 public class ApiDescriptionReader {
-    private static Logger log = LoggerFactory.getLogger(ApiDescriptionReader.class);
-    private final OperationReader operationReader;
-    private final DocumentationPluginsManager pluginsManager;
-    private final ApiDescriptionLookup lookup;
 
-    @Autowired
-    public ApiDescriptionReader(
-            @Qualifier("cachedOperations") OperationReader operationReader,
-            DocumentationPluginsManager pluginsManager,
-            ApiDescriptionLookup lookup) {
-        this.operationReader = operationReader;
-        this.pluginsManager = pluginsManager;
-        this.lookup = lookup;
-    }
+  private static Logger log = LoggerFactory.getLogger(ApiDescriptionReader.class);
+  private final OperationReader operationReader;
+  private final DocumentationPluginsManager pluginsManager;
+  private final ApiDescriptionLookup lookup;
 
-    public List<ApiDescription> read(RequestMappingContext outerContext) {
-        PatternsRequestCondition patternsCondition = outerContext.getPatternsCondition();
-        ApiSelector selector = outerContext.getDocumentationContext().getApiSelector();
+  @Autowired
+  public ApiDescriptionReader(
+          @Qualifier("cachedOperations") OperationReader operationReader,
+          DocumentationPluginsManager pluginsManager,
+          ApiDescriptionLookup lookup) {
+    this.operationReader = operationReader;
+    this.pluginsManager = pluginsManager;
+    this.lookup = lookup;
+  }
 
-        List<ApiDescription> apiDescriptionList = newArrayList();
-        for (String path : matchingPaths(selector, patternsCondition)) {
-            String methodName = outerContext.getName();
-            try {
-                RequestMappingContext operationContext = outerContext.copyPatternUsing(path);
+  public List<ApiDescription> read(RequestMappingContext outerContext) {
+    PatternsRequestCondition patternsCondition = outerContext.getPatternsCondition();
+    ApiSelector selector = outerContext.getDocumentationContext().getApiSelector();
 
-                List<Operation> operations = operationReader.read(operationContext);
-                if (operations.size() > 0) {
-                    operationContext.apiDescriptionBuilder()
-                            .operations(operations)
-                            .pathDecorator(pluginsManager.decorator(new PathContext(outerContext, from(operations).first())))
-                            .path(path)
-                            .description(methodName)
-                            .hidden(false);
-                    ApiDescription apiDescription = operationContext.apiDescriptionBuilder().build();
-                    lookup.add(outerContext.key(), apiDescription);
-                    apiDescriptionList.add(apiDescription);
-                }
-            } catch (Error e) {
-                String contentMsg = "Process path[" + path + "],method[" + methodName + "] error";
-                log.error(contentMsg, e);
-                throw new Error(contentMsg, e);
-            }
+    List<ApiDescription> apiDescriptionList = newArrayList();
+    for (String path : matchingPaths(selector, patternsCondition)) {
+      String methodName = outerContext.getName();
+      try {
+        RequestMappingContext operationContext = outerContext.copyPatternUsing(path);
+
+        List<Operation> operations = operationReader.read(operationContext);
+        if (operations.size() > 0) {
+          operationContext.apiDescriptionBuilder()
+                  .operations(operations)
+                  .pathDecorator(pluginsManager.decorator(new PathContext(outerContext, from(operations).first())))
+                  .path(path)
+                  .description(methodName)
+                  .hidden(false);
+          ApiDescription apiDescription = operationContext.apiDescriptionBuilder().build();
+          lookup.add(outerContext.key(), apiDescription);
+          apiDescriptionList.add(apiDescription);
         }
-        return apiDescriptionList;
+      } catch (Error e) {
+        String contentMsg = "Process path[" + path + "],method[" + methodName + "] error";
+        log.error(contentMsg, e);
+        throw new Error(contentMsg, e);
+      }
     }
+    return apiDescriptionList;
+  }
 
-    private List<String> matchingPaths(ApiSelector selector, PatternsRequestCondition patternsCondition) {
-        return natural().sortedCopy(from(patternsCondition.getPatterns())
-                .filter(selector.getPathSelector()));
-    }
+  private List<String> matchingPaths(ApiSelector selector, PatternsRequestCondition patternsCondition) {
+    return natural().sortedCopy(from(patternsCondition.getPatterns())
+            .filter(selector.getPathSelector()));
+  }
 
 }
