8c3474e8fda5c606f6bf25cb5cdfdf8fbfdefc65
ivan <ivan.les@2e-systems.com>
2018-03-01 11:33:10 +0100

    #1773 springfox: spring-boot:2.0.0.RELEASE with other dependencies upgraded to match those in boot

--- springfox-spring-web/src/main/java/springfox/documentation/spring/web/ControllerNamingUtils.java
+++ springfox-spring-web/src/main/java/springfox/documentation/spring/web/ControllerNamingUtils.java
@@ -25,8 +25,6 @@ import org.springframework.util.Assert;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.util.UriUtils;
 
-import java.io.UnsupportedEncodingException;
-
 import static springfox.documentation.spring.web.paths.Paths.splitCamelCase;
 
 public class ControllerNamingUtils {
@@ -50,12 +48,7 @@ public class ControllerNamingUtils {
   }
 
   public static String encode(String path) {
-    try {
       return UriUtils.encodePath(path, ISO_8859_1);
-    } catch (UnsupportedEncodingException e) {
-      log.error("Could not encode:" + path, e);
-    }
-    return path;
   }
 
   public static String decode(String path) {
