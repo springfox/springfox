1f16cd0a577123fd2a323e1d232aeb93c6714aca
Dilip Krishnan <D_KRISHNAN@containerstore.com>
2018-05-03 20:37:29 -0500

    Removed redundant tasks that arent required
    (2394)

--- buildSrc/src/main/groovy/springfox/gradlebuild/plugins/MultiProjectReleasePlugin.groovy
+++ buildSrc/src/main/groovy/springfox/gradlebuild/plugins/MultiProjectReleasePlugin.groovy
@@ -22,10 +22,15 @@ package springfox.gradlebuild.plugins
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.Task
-import springfox.gradlebuild.BintrayCredentials
 import springfox.gradlebuild.BuildInfo
 import springfox.gradlebuild.BuildInfoFactory
-import springfox.gradlebuild.tasks.*
+import springfox.gradlebuild.tasks.BumpAndTagTask
+import springfox.gradlebuild.tasks.CheckCleanWorkspaceTask
+import springfox.gradlebuild.tasks.CheckGitBranchTask
+import springfox.gradlebuild.tasks.IntermediaryTask
+import springfox.gradlebuild.tasks.PublishCredentialsCheckTask
+import springfox.gradlebuild.tasks.ReleaseTask
+import springfox.gradlebuild.tasks.SnapshotTask
 import springfox.gradlebuild.version.FileVersionStrategy
 import springfox.gradlebuild.version.ReleaseType
 import springfox.gradlebuild.version.VersioningStrategy
@@ -34,7 +39,7 @@ import springfox.gradlebuild.version.VersioningStrategy
  * https://www.youtube.com/watch?v=Y6SVoXFsw7I ( GradleSummit2014 - Releasing With Gradle - René Groeschke)
  *
  */
-public class MultiProjectReleasePlugin implements Plugin<Project> {
+class MultiProjectReleasePlugin implements Plugin<Project> {
 
   ReleaseTask releaseTask
   BumpAndTagTask bumpAndTagTask
@@ -62,7 +67,7 @@ public class MultiProjectReleasePlugin implements Plugin<Project> {
       description = 'Show project publishing information'
     }
 
-    configureVersionAndPublications(project, versioningInfo)
+    configureVersion(project, versioningInfo)
     configureSnapshotTaskGraph(project)
     configureReleaseTaskGraph(project)
     project.tasks.showPublishInfo << {
@@ -119,40 +124,9 @@ public class MultiProjectReleasePlugin implements Plugin<Project> {
     releaseTask.dependsOn bumpAndTagTask
   }
 
-  def configureVersionAndPublications(Project project, BuildInfo buildInfo) {
+  def configureVersion(Project project, BuildInfo buildInfo) {
     project.version = "${buildInfo.buildVersion.asText()}"
     project.ext.buildInfo = buildInfo
-
-    configurePublications(project, buildInfo)
-  }
-
-  def configurePublications(Project project, BuildInfo buildInfo) {
-    def isSnapshotBuild = isSnapshotBuild(project)
-    def type = isSnapshotBuild ? 'snapshot' : 'release'
-    def login = new BintrayCredentials(project)
-    def artifactRepoBase = 'http://oss.jfrog.org/artifactory'
-    def repoPrefix = 'oss'
-    project.ext {
-      bintrayCredentials = login
-      releaseRepos = {
-        //Only snapshots - bintray plugin takes care of non-snapshot releases
-        if (isSnapshotBuild) {
-          project.logger.lifecycle("[RELEASE] Setting up maven repo for snapshot build: $buildInfo")
-          maven {
-            name 'jfrogOss'
-            url "${artifactRepoBase}/${repoPrefix}-${type}-local"
-            credentials {
-              username = "${login.username}"
-              password = "${login.password}"
-            }
-          }
-        }
-      }
-    }
-  }
-
-  static boolean isSnapshotBuild(Project project) {
-    project.gradle.startParameter.taskNames.contains("snapshot")
   }
 
   static def createBuildInfo(Project project, VersioningStrategy versioningStrategy) {
