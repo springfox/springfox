f2ca5ead5c304aa7411797c8fae8bcc17868b210
Danny Bajé Joaquim <danny.bajejoaquim@outlook.com>
2018-06-26 00:19:56 +0200

    Reverting SpringVersionCapability as not usuable for this case

--- springfox-swagger-common/src/main/java/springfox/documentation/swagger/common/SpringVersionCapability.java
+++ springfox-swagger-common/src/main/java/springfox/documentation/swagger/common/SpringVersionCapability.java
@@ -22,7 +22,6 @@ public class SpringVersionCapability {
 
   private static final Version FIVE_ZERO_ZERO = Version.parse("5.0.0.RELEASE");
   private static final Version FIVE_ZERO_FIVE = Version.parse("5.0.5.RELEASE");
-  private static final Version FOUR_THREE_THREE = Version.parse("4.3.3.RELEASE");
   private static final Version FOUR_THREE_FIFTEEN = Version.parse("4.3.15.RELEASE");
 
   public SpringVersionCapability() {
@@ -35,10 +34,5 @@ public class SpringVersionCapability {
     return (parsed.isGreaterThanOrEqualTo(FOUR_THREE_FIFTEEN)
                 && parsed.isLessThan(FIVE_ZERO_ZERO)) || 
         parsed.isGreaterThanOrEqualTo(FIVE_ZERO_FIVE);
-  }
-
-  public static boolean supportsExtendedPathVariableAnnotation(String version) {
-    Version parsed = Version.parse(version);
-    return parsed.isGreaterThanOrEqualTo(FOUR_THREE_THREE);
   }
 }
