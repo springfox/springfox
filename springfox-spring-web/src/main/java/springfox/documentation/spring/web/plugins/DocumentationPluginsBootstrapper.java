30524433da19567084b76ff0ea81077495e2d64d
Esteban <ecristobalr@gmail.com>
2018-06-26 14:52:12 +0200

    Rolled back change as the entry point defined on previous commit will not be used to implement the solution

--- springfox-spring-web/src/main/java/springfox/documentation/spring/web/plugins/DocumentationPluginsBootstrapper.java
+++ springfox-spring-web/src/main/java/springfox/documentation/spring/web/plugins/DocumentationPluginsBootstrapper.java
@@ -170,7 +170,6 @@ public class DocumentationPluginsBootstrapper implements SmartLifecycle {
               documentationType.getName(), documentationType.getVersion());
         }
       }
-      //TODO Entry point for Swagger files upload
     }
   }
 
