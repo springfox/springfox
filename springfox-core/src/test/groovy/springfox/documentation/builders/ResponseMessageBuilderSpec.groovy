74baed261902d4e1fb040c6192e3a5835c09cd6a
Solohub Serhii <sergei.sologub@gmail.com>
2018-06-26 13:01:28 +0300

    Update response message tests with examples values

--- springfox-core/src/test/groovy/springfox/documentation/builders/ResponseMessageBuilderSpec.groovy
+++ springfox-core/src/test/groovy/springfox/documentation/builders/ResponseMessageBuilderSpec.groovy
@@ -37,10 +37,11 @@ class ResponseMessageBuilderSpec extends Specification {
       built."$property" == value
 
     where:
-      builderMethod     | value                  | property
-      'code'            | 200                    | 'code'
-      'message'         | 'OK'                   | 'message'
-      'responseModel'   | new ModelRef('String') | 'responseModel'
+      builderMethod   | value                                 | property
+      'code'          | 200                                   | 'code'
+      'message'       | 'OK'                                  | 'message'
+      'responseModel' | new ModelRef('String')                | 'responseModel'
+      'examples'      | ['application/json': '{"foo":"bar"}'] | 'examples'
   }
 
   def "Setting builder properties to null values preserves existing values"() {
@@ -55,9 +56,10 @@ class ResponseMessageBuilderSpec extends Specification {
       built."$property" == value
 
     where:
-      builderMethod     | value                  | property
-      'message'         | 'OK'                   | 'message'
-      'responseModel'   | new ModelRef('String') | 'responseModel'
+      builderMethod     | value                                     | property
+      'message'         | 'OK'                                      | 'message'
+      'responseModel'   | new ModelRef('String')                    | 'responseModel'
+      'examples'        | ['application/json': '{"foo":"bar"}']     | 'examples'
   }
 
 
