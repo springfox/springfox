83579f6c8202e0be04df52c55a3ddee6af6dcf95
Danny Bajé Joaquim <danny.bajejoaquim@outlook.com>
2018-06-28 00:32:43 +0200

    Trying to solve style check error

--- springfox-core/src/main/java/springfox/documentation/common/SpringVersion.java
+++ springfox-core/src/main/java/springfox/documentation/common/SpringVersion.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package springfox.documentation.common;
 
 /**
@@ -12,6 +28,15 @@ public class SpringVersion {
      * @see Package#getImplementationVersion()
      */
     public Version getVersion() {
-        return Version.parse(org.springframework.core.SpringVersion.getVersion());
+        return Version.parse(getVersionString());
+    }
+
+    /**
+     * Return the full version string of the present Spring codebase,
+     * or {@code null} if it cannot be determined.
+     * @see Package#getImplementationVersion()
+     */
+    private String getVersionString() {
+        return org.springframework.core.SpringVersion.getVersion();
     }
 }
