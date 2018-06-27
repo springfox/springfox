07c75a4184fb77c8fe78d059584a8d3c149301af
Esteban <ecristobalr@gmail.com>
2018-06-27 15:33:34 +0200

    Implemented uploading to SwaggerHub through its REST API

--- springfox-swaggerhub/src/main/java/springfox/documentation/swaggerhub/SwaggerHubApiUploader.java
+++ springfox-swaggerhub/src/main/java/springfox/documentation/swaggerhub/SwaggerHubApiUploader.java
@@ -22,10 +22,15 @@ import io.swagger.models.Swagger;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import springfox.documentation.service.Documentation;
 import springfox.documentation.spring.web.DocumentationCache;
+import springfox.documentation.spring.web.json.Json;
+import springfox.documentation.spring.web.json.JsonSerializer;
 import springfox.documentation.swagger2.mappers.ServiceModelToSwagger2Mapper;
 
 import javax.annotation.PostConstruct;
@@ -36,18 +41,30 @@ public class SwaggerHubApiUploader {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SwaggerHubApiUploader.class);
 
+    private static final String SWAGGERHUB_URL = "https://swaggerhub.com/api/{owner}/{api}";
+
     private DocumentationCache documentationCache;
 
     private ServiceModelToSwagger2Mapper mapper;
 
+    private JsonSerializer jsonSerializer;
+
     private RestTemplate restTemplate;
+
+    //TODO Get value from application.properties
+    private String swaggerHubApiKey;
+
+    //TODO Get value from application.properties
+    private String swaggerHubOwner;
 
     @Autowired
     public SwaggerHubApiUploader(
             final DocumentationCache documentationCache,
-            final ServiceModelToSwagger2Mapper mapper) {
+            final ServiceModelToSwagger2Mapper mapper,
+            final JsonSerializer jsonSerializer) {
         this.documentationCache = documentationCache;
         this.mapper = mapper;
+        this.jsonSerializer = jsonSerializer;
         this.restTemplate = new RestTemplate();
     }
 
@@ -56,10 +73,17 @@ public class SwaggerHubApiUploader {
         LOGGER.debug("Start Swagger file upload to SwaggerHub");
         final Map<String, Documentation> documentationMap = this.documentationCache.all();
         for (Map.Entry<String, Documentation> entry : documentationMap.entrySet()) {
+            final String apiName = entry.getKey();
             final Swagger swagger = this.mapper.mapDocumentation(entry.getValue());
-            LOGGER.debug("Uploading Swagger file for {} endpoint", entry.getKey());
-            //TODO Implement file upload
-            LOGGER.debug("Successfully uploaded Swagger file for {} endpoint", entry.getKey());
+            final Json jsonSwagger = this.jsonSerializer.toJson(swagger);
+            LOGGER.debug("Uploading Swagger file for {} endpoint", apiName);
+            final HttpHeaders headers = new HttpHeaders();
+            headers.add("Authorization", "Basic ".concat(this.swaggerHubApiKey));
+            final HttpEntity<Json> request = new HttpEntity<Json>(jsonSwagger, headers);
+            final ResponseEntity<String> response = this.restTemplate
+                    .postForEntity(SWAGGERHUB_URL, request, String.class, this.swaggerHubOwner, apiName);
+            //TODO Parse received response
+            LOGGER.debug("Successfully uploaded Swagger file for {} endpoint", apiName);
         }
         LOGGER.debug("Finished Swagger file upload to SwaggerHub. Uploaded {} files", documentationMap.size());
     }
