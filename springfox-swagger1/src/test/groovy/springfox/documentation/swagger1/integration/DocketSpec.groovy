d43100ef8b7b65036cfe08b0c5781daca2ff9ed3
Dilip Krishnan <D_KRISHNAN@containerstore.com>
2018-08-18 09:06:19 -0500

    Reformatted test
    (1773)

--- springfox-swagger1/src/test/groovy/springfox/documentation/swagger1/integration/DocketSpec.groovy
+++ springfox-swagger1/src/test/groovy/springfox/documentation/swagger1/integration/DocketSpec.groovy
@@ -47,133 +47,141 @@ class DocketSpec extends DocumentationContextSpec {
 
   def "Should have sensible defaults when built with minimal configuration"() {
     when:
-      plugin.configure(contextBuilder)
-      def pluginContext = contextBuilder.build()
+    plugin.configure(contextBuilder)
+    def pluginContext = contextBuilder.build()
+
     then:
-      pluginContext.groupName == 'default'
-      pluginContext.securitySchemes == null
-      pluginContext.apiInfo.getTitle() == "Api Documentation"
-      pluginContext.apiInfo.getDescription() == "Api Documentation"
-      pluginContext.apiInfo.getTermsOfServiceUrl() == 'urn:tos'
+    pluginContext.groupName == 'default'
+    pluginContext.securitySchemes == null
+    pluginContext.apiInfo.getTitle() == "Api Documentation"
+    pluginContext.apiInfo.getDescription() == "Api Documentation"
+    pluginContext.apiInfo.getTermsOfServiceUrl() == 'urn:tos'
 
-      def contact = pluginContext.apiInfo.getContact()
-      contact.name == ''
-      contact.email == ''
-      contact.url == ''
-      pluginContext.apiInfo.getLicense() == 'Apache 2.0'
-      pluginContext.apiInfo.getLicenseUrl() == "http://www.apache.org/licenses/LICENSE-2.0"
-      pluginContext.apiInfo.version == "1.0"
+    def contact = pluginContext.apiInfo.getContact()
+    contact.name == ''
+    contact.email == ''
+    contact.url == ''
+    pluginContext.apiInfo.getLicense() == 'Apache 2.0'
+    pluginContext.apiInfo.getLicenseUrl() == "http://www.apache.org/licenses/LICENSE-2.0"
+    pluginContext.apiInfo.version == "1.0"
 
-      pluginContext.pathProvider instanceof DummyPathProvider // this one is dummy as this is what we have in test ctx
+    pluginContext.pathProvider instanceof DummyPathProvider // this one is dummy as this is what we have in test ctx
   }
 
   def "Swagger global response messages should override the default for a particular RequestMethod"() {
     when:
-      plugin
-              .globalResponseMessage(GET, [new ResponseMessage(
-          OK.value(),
-          "blah",
-          null,
-          [],
-          [] as Map,
-          [])])
-              .useDefaultResponseMessages(true)
-              .configure(contextBuilder)
+    plugin
+        .globalResponseMessage(GET, [new ResponseMessage(
+        OK.value(),
+        "blah",
+        null,
+        [],
+        [] as Map,
+        [])])
+        .useDefaultResponseMessages(true)
+        .configure(contextBuilder)
 
     and:
-      def pluginContext = contextBuilder.build()
+    def pluginContext = contextBuilder.build()
+
     then:
-      pluginContext.getGlobalResponseMessages()[GET][0].getMessage() == "blah"
-      pluginContext.getGlobalResponseMessages()[GET].size() == 1
+    pluginContext.getGlobalResponseMessages()[GET][0].getMessage() == "blah"
+    pluginContext.getGlobalResponseMessages()[GET].size() == 1
 
     and: "defaults are preserved"
-      pluginContext.getGlobalResponseMessages().keySet().containsAll(
-              [POST, PUT, DELETE, PATCH, TRACE, OPTIONS, HEAD]
-      )
+    pluginContext.getGlobalResponseMessages().keySet().containsAll(
+        [POST, PUT, DELETE, PATCH, TRACE, OPTIONS, HEAD]
+    )
   }
 
   def "Swagger global response messages should not be used for a particular RequestMethod"() {
     when:
-      new Docket(DocumentationType.SWAGGER_12)
-              .globalResponseMessage(GET, [new ResponseMessage(
-          OK.value(),
-          "blah",
-          null,
-          [],
-          [] as Map,
-          [])])
-              .useDefaultResponseMessages(false)
-              .configure(contextBuilder)
+    new Docket(DocumentationType.SWAGGER_12)
+        .globalResponseMessage(GET, [new ResponseMessage(
+        OK.value(),
+        "blah",
+        null,
+        [],
+        [] as Map,
+        [])])
+        .useDefaultResponseMessages(false)
+        .configure(contextBuilder)
 
     and:
-      def pluginContext = contextBuilder.build()
+    def pluginContext = contextBuilder.build()
+
     then:
-      pluginContext.getGlobalResponseMessages()[GET][0].getMessage() == "blah"
-      pluginContext.getGlobalResponseMessages()[GET].size() == 1
+    pluginContext.getGlobalResponseMessages()[GET][0].getMessage() == "blah"
+    pluginContext.getGlobalResponseMessages()[GET].size() == 1
 
     and: "defaults are preserved"
-      pluginContext.getGlobalResponseMessages().keySet().containsAll([GET])
+    pluginContext.getGlobalResponseMessages().keySet().containsAll([GET])
   }
 
   def "Swagger ignorableParameterTypes should append to the default ignorableParameterTypes"() {
     when:
-      new Docket(DocumentationType.SWAGGER_12)
-              .ignoredParameterTypes(AbstractSingletonProxyFactoryBean.class, ProxyFactoryBean.class)
-              .configure(contextBuilder)
+    new Docket(DocumentationType.SWAGGER_12)
+        .ignoredParameterTypes(AbstractSingletonProxyFactoryBean.class, ProxyFactoryBean.class)
+        .configure(contextBuilder)
+
     and:
-      def pluginContext = contextBuilder.build()
+    def pluginContext = contextBuilder.build()
+
     then:
-      pluginContext.getIgnorableParameterTypes().contains(AbstractSingletonProxyFactoryBean.class)
-      pluginContext.getIgnorableParameterTypes().contains(ProxyFactoryBean.class)
+    pluginContext.getIgnorableParameterTypes().contains(AbstractSingletonProxyFactoryBean.class)
+    pluginContext.getIgnorableParameterTypes().contains(ProxyFactoryBean.class)
 
     and: "one of the defaults"
-      pluginContext.getIgnorableParameterTypes().contains(ServletRequest.class)
+    pluginContext.getIgnorableParameterTypes().contains(ServletRequest.class)
   }
 
   def "Sets alternative AlternateTypeProvider with a rule"() {
     given:
-      def rule = newMapRule(String, String)
-      new Docket(DocumentationType.SWAGGER_12)
-              .alternateTypeRules(rule)
-              .configure(contextBuilder)
+    def rule = newMapRule(String, String)
+    new Docket(DocumentationType.SWAGGER_12)
+        .alternateTypeRules(rule)
+        .configure(contextBuilder)
+
     expect:
-      documentationContext().alternateTypeProvider.rules.contains(rule)
+    documentationContext().alternateTypeProvider.rules.contains(rule)
   }
 
   def "Model substitution registers new rules"() {
     when:
-      def swaggerDefault = new SwaggerDefaultConfiguration(new Defaults(), new TypeResolver(), Mock(PathProviderFactory))
-              .create(DocumentationType.SWAGGER_12)
-      def isjdk8 = System.getProperty("java.version").startsWith("1.8")
-      def jdk8RuleCount = (isjdk8 ? 6 : 0)
+    def swaggerDefault = new SwaggerDefaultConfiguration(new Defaults(), new TypeResolver(), Mock(PathProviderFactory))
+        .create(DocumentationType.SWAGGER_12)
+    def isjdk8 = System.getProperty("java.version").startsWith("1.8")
+    def jdk8RuleCount = (isjdk8 ? 6 : 0)
+
     and:
-      new Docket(DocumentationType.SWAGGER_12)
-              ."${method}"(*args)
-              .configure(swaggerDefault)
+    new Docket(DocumentationType.SWAGGER_12)
+        ."${method}"(*args)
+        .configure(swaggerDefault)
+
     then:
-      swaggerDefault.build().alternateTypeProvider.rules.size() == expectedSize + jdk8RuleCount
+    swaggerDefault.build().alternateTypeProvider.rules.size() == expectedSize + jdk8RuleCount
 
     where:
-      method                    | args                               | expectedSize
-      'genericModelSubstitutes' | [ResponseEntity.class, List.class] | 17
-      'directModelSubstitute'   | [LocalDate.class, Date.class]      | 16
+    method                    | args                               | expectedSize
+    'genericModelSubstitutes' | [ResponseEntity.class, List.class] | 17
+    'directModelSubstitute'   | [LocalDate.class, Date.class]      | 16
   }
 
   def "Basic property checks"() {
     when:
-      plugin."$builderMethod"(object)
+    plugin."$builderMethod"(object)
 
     then:
-      documentationContext()."$property" == object
+    documentationContext()."$property" == object
 
     where:
-      builderMethod     | object                                  | property
-      'pathProvider'    | new WebMvcRelativePathProvider()        | 'pathProvider'
-      'securitySchemes' | new ArrayList<SecurityScheme>()         | 'securitySchemes'
-      'securityContexts'| validContexts()                         | 'securityContexts'
-      'groupName'       | 'someGroup'                             | 'groupName'
-      'apiInfo'         | new ApiInfo('', '', "", '', '', '', '') | 'apiInfo'
-      'apiInfo'         | ApiInfo.DEFAULT                         | 'apiInfo'
+    builderMethod      | object                                  | property
+    'pathProvider'     | new WebMvcRelativePathProvider()        | 'pathProvider'
+    'securitySchemes'  | new ArrayList<SecurityScheme>()         | 'securitySchemes'
+    'securityContexts' | validContexts()                         | 'securityContexts'
+    'groupName'        | 'someGroup'                             | 'groupName'
+    'apiInfo'          | new ApiInfo('', '', "", '', '', '', '') | 'apiInfo'
+    'apiInfo'          | ApiInfo.DEFAULT                         | 'apiInfo'
   }
 
   def validContexts() {
@@ -183,20 +191,21 @@ class DocketSpec extends DocumentationContextSpec {
   def "non nullable swaggerApiResourceListing properties"() {
 
     when:
-      new Docket(DocumentationType.SWAGGER_12)
-              .configure(contextBuilder)
+    new Docket(DocumentationType.SWAGGER_12)
+        .configure(contextBuilder)
 
     and:
-      def pluginContext = contextBuilder.build()
+    def pluginContext = contextBuilder.build()
+
     then:
-      "default" == pluginContext.groupName
-      null != pluginContext.pathProvider
-      null != pluginContext.apiInfo
-      null != pluginContext.apiSelector
-      null != pluginContext.globalResponseMessages
-      null != pluginContext.ignorableParameterTypes
-      null != pluginContext.listingReferenceOrdering
-      null != pluginContext.apiDescriptionOrdering
+    "default" == pluginContext.groupName
+    null != pluginContext.pathProvider
+    null != pluginContext.apiInfo
+    null != pluginContext.apiSelector
+    null != pluginContext.globalResponseMessages
+    null != pluginContext.ignorableParameterTypes
+    null != pluginContext.listingReferenceOrdering
+    null != pluginContext.apiDescriptionOrdering
 
   }
 }
