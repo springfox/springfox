160da78bdfd4880182c2c7961738428525ad0992
ecristobal <ecristobal@users.noreply.github.com>
2018-09-18 17:10:53 +0200

    Added comments to annotation

--- springfox-uploader/src/main/java/springfox/documentation/uploader/annotations/EnableSwaggerUpload.java
+++ springfox-uploader/src/main/java/springfox/documentation/uploader/annotations/EnableSwaggerUpload.java
@@ -7,6 +7,12 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+/**
+ * Indicates that auto upload should be enabled. This should be applied to a Spring Java config file and should have an
+ * accompanying '@Configuration' annotation. Loads all required beans defined in @see FileUploaderBeanConfiguration.
+ *
+ * @author Esteban Cristóbal Rodríguez
+ **/
 @Retention(value = java.lang.annotation.RetentionPolicy.RUNTIME)
 @Target(value = {java.lang.annotation.ElementType.TYPE})
 @Documented
