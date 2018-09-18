dd4c07b8f9b1a8e54efeffd5c9f28cd5578ac9b3
ecristobal <ecristobal@users.noreply.github.com>
2018-09-18 17:15:13 +0200

    Added comments to class

--- springfox-uploader/src/main/java/springfox/documentation/uploader/FileUploader.java
+++ springfox-uploader/src/main/java/springfox/documentation/uploader/FileUploader.java
@@ -4,7 +4,17 @@ import springfox.documentation.service.Documentation;
 
 import java.util.Map;
 
+/**
+ * Interface to be implemented by any provider who aims to be supported by the auto-upload feature.
+ *
+ * @author Esteban Cristóbal Rodríguez
+ */
 public interface FileUploader {
 
+    /**
+     * Uploads the descriptors passed as parameter to the provider implementing this interface.
+     *
+     * @param descriptors Swagger descriptors (values) to be uploaded, and endpoints (keys) they belong to.
+     */
     void uploadSwaggerDescriptors(Map<String, Documentation> descriptors);
 }
