b91eb5bd077494866eeecd8e449aaf36f4553d7e
Dietrich Schulten <ds@escalon.de>
2018-11-21 23:13:38 +0100

    reverted changes to SwaggerApplication

--- swagger-contract-tests/src/main/java/springfox/test/contract/swagger/SwaggerApplication.java
+++ swagger-contract-tests/src/main/java/springfox/test/contract/swagger/SwaggerApplication.java
@@ -21,10 +21,8 @@ package springfox.test.contract.swagger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @SpringBootApplication
-@EnableSwagger2WebMvc
 public class SwaggerApplication {
   public static void main(String[] args) {
     SpringApplication.run(SwaggerApplication.class, args);
