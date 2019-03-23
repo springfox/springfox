71ef6d52270f927c49b32e6c1c1771abdd2d7c64
Dilip Krishnan <D_KRISHNAN@containerstore.com>
2019-03-23 10:34:51 -0500

    Enriched Example and added builders
    Kept the constructors and deprecated them for backwards compatibility
    
    (2022)

--- springfox-core/src/main/java/springfox/documentation/schema/Example.java
+++ springfox-core/src/main/java/springfox/documentation/schema/Example.java
@@ -18,24 +18,89 @@
  */
 package springfox.documentation.schema;
 
+import springfox.documentation.service.VendorExtension;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.StringJoiner;
 
 import static java.util.Optional.*;
 
 public class Example {
+  private final String id;
+  private final String summary;
+  private final String description;
   private final Object value;
+  private final String externalValue;
   private final String mediaType;
+  private final List<VendorExtension> extensions = new ArrayList<>();
 
+  /**
+   * @deprecated @since 3.0.0
+   * Use @see {@link Example#Example(String, String, String, Object, String, String)}
+   * @param value
+   */
+  @Deprecated
   public Example(Object value) {
     this.value = value;
     this.mediaType = null;
+    externalValue = null;
+    id = null;
+    summary = null;
+    description = null;
   }
 
+  /**
+   * @deprecated @since 3.0.0
+   * Use @see {@link Example#Example(String, String, String, Object, String, String)}
+   * @param value
+   */
+  @Deprecated
   public Example(
       String mediaType,
       Object value) {
     this.mediaType = mediaType;
     this.value = value;
+    externalValue = null;
+    id = null;
+    summary = null;
+    description = null;
+  }
+
+  public Example(
+      String id,
+      String summary,
+      String description,
+      Object value,
+      String externalValue,
+      String mediaType) {
+    this.id = id;
+    this.summary = summary;
+    this.description = description;
+    this.value = value;
+    this.externalValue = externalValue;
+    this.mediaType = mediaType;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public String getSummary() {
+    return summary;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public String getExternalValue() {
+    return externalValue;
+  }
+
+  public List<VendorExtension> getExtensions() {
+    return extensions;
   }
 
   public Object getValue() {
@@ -48,6 +113,14 @@ public class Example {
 
   @Override
   public String toString() {
-    return String.valueOf(value);
+    return new StringJoiner(", ", Example.class.getSimpleName() + "[", "]")
+        .add("id='" + id + "'")
+        .add("summary='" + summary + "'")
+        .add("description='" + description + "'")
+        .add("value=" + value)
+        .add("externalValue='" + externalValue + "'")
+        .add("mediaType='" + mediaType + "'")
+        .add("extensions=" + extensions)
+        .toString();
   }
 }
