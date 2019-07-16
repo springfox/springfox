808a94865336d876c666758ea014408fdac39c64
liuxy <391861737@qq.com>
2019-07-16 09:53:53 +0800

    Add licenses

--- springfox-swagger-common/src/test/groovy/springfox/documentation/swagger/web/ApiResourceControllerCsrfWebMvcSpec.groovy
+++ springfox-swagger-common/src/test/groovy/springfox/documentation/swagger/web/ApiResourceControllerCsrfWebMvcSpec.groovy
@@ -1,3 +1,21 @@
+/*
+ *
+ *  Copyright 2017-2019 the original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ */
 package springfox.documentation.swagger.web
 
 import org.mockito.Mockito
@@ -5,7 +23,6 @@ import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
-import spock.lang.Shared
 import springfox.documentation.swagger.common.ClassUtils
 import springfox.documentation.swagger.csrf.CsrfStrategy
 
