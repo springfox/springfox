06e1ca229498fde54c15bb74e9534cc650452f79
Wojciech Sołtys <wojciech.soltys@sap.com>
2019-07-31 08:21:33 +0200

    3062: Remove unnecessary import

--- springfox-spring-web/src/main/java/springfox/documentation/spring/web/plugins/Docket.java
+++ springfox-spring-web/src/main/java/springfox/documentation/spring/web/plugins/Docket.java
@@ -55,7 +55,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
-import java.util.TreeSet;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
