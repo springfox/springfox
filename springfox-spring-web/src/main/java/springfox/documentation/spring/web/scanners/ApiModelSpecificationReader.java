21333be783c6cc09c33228070f0cf589c8811754
Dilip Krishnan <dilipkrish_subscriptions@yahoo.com>
2020-05-14 05:14:37 -0500

    Created an ApiModelSpecificationReader
    This is a separate path to read the model specifications
    
    (2022)

---
+++ springfox-spring-web/src/main/java/springfox/documentation/spring/web/scanners/ApiModelSpecificationReader.java
@@ -0,0 +1,58 @@
+package springfox.documentation.spring.web.scanners;
+
+import com.fasterxml.classmate.TypeResolver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import springfox.documentation.schema.ModelSpecification;
+import springfox.documentation.schema.ModelSpecificationProvider;
+import springfox.documentation.spi.schema.contexts.ModelContext;
+import springfox.documentation.spi.service.contexts.RequestMappingContext;
+import springfox.documentation.spring.web.plugins.DocumentationPluginsManager;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+public class ApiModelSpecificationReader {
+  private static final Logger LOGGER = LoggerFactory.getLogger(ApiModelReader.class);
+  private final ModelSpecificationProvider modelProvider;
+  private final DocumentationPluginsManager pluginsManager;
+  private final TypeResolver resolver;
+
+  @Autowired
+  public ApiModelSpecificationReader(
+      @Qualifier("cachedModels") ModelSpecificationProvider modelProvider,
+      DocumentationPluginsManager pluginsManager,
+      TypeResolver resolver) {
+    this.modelProvider = modelProvider;
+    this.pluginsManager = pluginsManager;
+    this.resolver = resolver;
+  }
+
+  @SuppressWarnings("rawtypes")
+  public Set<ModelSpecification> read(RequestMappingContext context) {
+    Set<ModelSpecification> specifications = new HashSet<>();
+    Set<ModelContext> modelContexts = pluginsManager.modelContexts(context);
+    for (ModelContext each : modelContexts) {
+      markIgnorablesAsHasSeen(
+          context.getIgnorableParameterTypes(),
+          each);
+      modelProvider.modelSpecificationsFor(each)
+                   .ifPresent(specifications::add);
+    }
+    return specifications;
+  }
+
+  private void markIgnorablesAsHasSeen(
+      Set<Class> ignorableParameterTypes,
+      ModelContext modelContext) {
+
+    for (Class ignorableParameterType : ignorableParameterTypes) {
+      modelContext.seen(resolver.resolve(ignorableParameterType));
+    }
+  }
+
+}
