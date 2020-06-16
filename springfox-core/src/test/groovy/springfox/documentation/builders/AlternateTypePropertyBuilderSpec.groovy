b34ddb3beba0e3178bbecffeca0415a90ecd6421
Dilip Krishnan <dilipkrish_subscriptions@yahoo.com>
2020-06-16 10:18:08 -0500

    Added properties to increase coverage
    (2022)

--- springfox-core/src/test/groovy/springfox/documentation/builders/AlternateTypePropertyBuilderSpec.groovy
+++ springfox-core/src/test/groovy/springfox/documentation/builders/AlternateTypePropertyBuilderSpec.groovy
@@ -6,24 +6,28 @@ import spock.lang.Unroll
 
 class AlternateTypePropertyBuilderSpec extends Specification {
   @Unroll
-  def "Builds bean class with field with getter #canRead and setter #canWrite" () {
+  def "Builds bean class with field with getter #canRead and setter #canWrite"() {
     given:
-      def sut = new AlternateTypeBuilder()
+    def sut = new AlternateTypeBuilder()
+
     when:
-       def built = sut
-          .fullyQualifiedClassName(className)
-          .property(property(propertyName, canRead, canWrite))
-          .build()
+    def built = sut
+        .fullyQualifiedClassName(className)
+        .property(property(propertyName, canRead, canWrite))
+        .annotations([])
+        .build()
+
     then:
-      built.declaredFields.find { it.name == propertyName } != null
-      hasMethod(built, "get$propertyName") == canRead
-      hasMethod(built, "set$propertyName") == canWrite
+    built.declaredFields.find { it.name == propertyName } != null
+    hasMethod(built, "get$propertyName") == canRead
+    hasMethod(built, "set$propertyName") == canWrite
+
     where:
-      className       | propertyName  | canRead | canWrite
-      "a.b.NewClass"  | "test"        | false   | false
-      "a.b.NewClass"  | "test"        | true    | false
-      "a.b.NewClass"  | "test"        | false   | true
-      "a.b.NewClass"  | "test"        | true    | true
+    className      | propertyName | canRead | canWrite
+    "a.b.NewClass" | "test"       | false   | false
+    "a.b.NewClass" | "test"       | true    | false
+    "a.b.NewClass" | "test"       | false   | true
+    "a.b.NewClass" | "test"       | true    | true
   }
 
   def hasMethod(Class<?> built, methodName) {
