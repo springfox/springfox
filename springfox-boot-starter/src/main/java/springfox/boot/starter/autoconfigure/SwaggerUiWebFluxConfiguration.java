278323eb98f044651682b561a02478314943efd9
Dilip Krishnan <dilipkrish_subscriptions@yahoo.com>
2020-06-21 17:41:04 -0500

    Conditionally enable swagger-ui
    (2022)

--- springfox-boot-starter/src/main/java/springfox/boot/starter/autoconfigure/SwaggerUiWebFluxConfiguration.java
+++ springfox-boot-starter/src/main/java/springfox/boot/starter/autoconfigure/SwaggerUiWebFluxConfiguration.java
@@ -1,6 +1,7 @@
 package springfox.boot.starter.autoconfigure;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.util.StringUtils;
@@ -8,6 +9,10 @@ import org.springframework.util.StringUtils;
 import static springfox.documentation.builders.BuilderDefaults.*;
 
 @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.REACTIVE)
+@ConditionalOnProperty(
+    value = "springfox.documentation.swagger-ui.enabled",
+    havingValue = "true",
+    matchIfMissing = true)
 public class SwaggerUiWebFluxConfiguration {
   @Value("${springfox.documentation.swagger-ui.base-url:}")
   private String swaggerBaseUrl;
--- springfox-boot-starter/src/main/java/springfox/boot/starter/autoconfigure/SwaggerUiWebMvcConfiguration.java
+++ springfox-boot-starter/src/main/java/springfox/boot/starter/autoconfigure/SwaggerUiWebMvcConfiguration.java
@@ -1,6 +1,7 @@
 package springfox.boot.starter.autoconfigure;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,10 @@ import static springfox.documentation.builders.BuilderDefaults.*;
 
 @Configuration
 @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
+@ConditionalOnProperty(
+    value = "springfox.documentation.swagger-ui.enabled",
+    havingValue = "true",
+    matchIfMissing = true)
 public class SwaggerUiWebMvcConfiguration {
   @Value("${springfox.documentation.swagger-ui.base-url:}")
   private String swaggerBaseUrl;
