2026ff1ab2a65ba28c6fb0b8fd5a88ac0d205ce7
Dilip Krishnan <dilipkrish_subscriptions@yahoo.com>
2020-06-22 08:43:33 -0500

    Changed the command to include all as gradle properties

--- release.sh
+++ release.sh
@@ -1,1 +1,9 @@
-GITHUB_TOKEN=$GITHUB_TOKEN ./gradlew release publishDocs -PbintrayUsername=$BINTRAY_USER_NAME -PbintrayPassword=$BINTRAY_PASSWORD -i -x check
+echo  "Using GITHUB_TOKEN=$GITHUB_TOKEN, "\
+      "BINTRAY_USER_NAME=$BINTRAY_USER_NAME, "\
+      "BINTRAY_PASSWORD=$BINTRAY_PASSWORD, "\
+      "DRYRUN=$DRYRUN"
+./gradlew release publishDocs \
+  -PgithubToken=$GITHUB_TOKEN \
+  -PbintrayUsername=$BINTRAY_USER_NAME \
+  -PbintrayPassword=$BINTRAY_PASSWORD \
+  -PdryRun=$DRYRUN-i -x check
