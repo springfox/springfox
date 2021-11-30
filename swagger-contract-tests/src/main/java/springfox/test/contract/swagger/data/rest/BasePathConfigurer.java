c00c66f6e5ac5e8f19dad17c7024a9fb09390058
mbazos <michael.bazos@priceline.com>
2021-11-29 21:27:07 -0500

    attempting to fix tests

--- swagger-contract-tests/src/main/java/springfox/test/contract/swagger/data/rest/BasePathConfigurer.java
+++ swagger-contract-tests/src/main/java/springfox/test/contract/swagger/data/rest/BasePathConfigurer.java
@@ -22,14 +22,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
 public class BasePathConfigurer {
   @Bean
   public RepositoryRestConfigurer repositoryRestConfigurer() {
     return new RepositoryRestConfigurer() {
-      @Override
-      public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+      public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         config.setBasePath("/rest");
       }
     };
